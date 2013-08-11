e7f8bfc266773e78ff70d2d119572b0b296aeef3
lijealso <lijealso@gmail.com>
2013-08-11 11:23:08 +0100

    para corrigir conflito com redmine_contacts
    no menú principal > contactos - ao clicar no nome do contacto dava
    conflito com plugins contact_form - dava erro no URL ao invés de ir para
    o contacto respectivo no modo de adicionar nota

--- plugins/contact_form/config/routes.rb
+++ plugins/contact_form/config/routes.rb
@@ -1,20 +1,20 @@
 if Rails::VERSION::MAJOR < 3
 
     ActionController::Routing::Routes.draw do |map|
-        map.connect('contact/preview', :controller => 'contact_form', :action => 'preview')
-        map.connect('contact/mail/:id', :controller => 'contact_form', :action => 'mail')
-        map.connect('contact/:id', :controller => 'contact_form', :action => 'index')
-        map.connect('contact', :controller => 'contact_form', :action => 'index')
-        map.connect('settings/contact', :controller => 'contact_form', :action => 'settings')
+        map.connect('contacte/preview', :controller => 'contact_form', :action => 'preview')
+        map.connect('contacte/mail/:id', :controller => 'contact_form', :action => 'mail')
+        map.connect('contacte/:id', :controller => 'contact_form', :action => 'index')
+        map.connect('contacte', :controller => 'contact_form', :action => 'index')
+        map.connect('settings/contacte', :controller => 'contact_form', :action => 'settings')
     end
 
 else
 
-    match 'contact/preview', :to => 'contact_form#preview'
-    match 'contact/mail/:id', :to => 'contact_form#mail'
-    match 'contact/mail', :to => 'contact_form#mail'
-    match 'contact/:id', :to => 'contact_form#index'
-    match 'contact', :to => 'contact_form#index'
-    match 'settings/contact', :to => 'contact_form#settings'
+    match 'contacte/preview', :to => 'contact_form#preview'
+    match 'contacte/mail/:id', :to => 'contact_form#mail'
+    match 'contacte/mail', :to => 'contact_form#mail'
+    match 'contacte/:id', :to => 'contact_form#index'
+    match 'contacte', :to => 'contact_form#index'
+    match 'settings/contacte', :to => 'contact_form#settings'
 
 end
